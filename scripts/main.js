fa7c152206da5a24e29e58edef420a3adbed2a26
Vladimir Marsov <marsov012@gmail.com>
2021-02-01 19:36:31 +0200

    Сделать форму и READme

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,22 @@ let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__btn-close');
 let heading = document.querySelector('.profile__heading');
 let headingDescription = document.querySelector('.profile__heading-description');
+
+let formElement = popup.querySelector('.popup__form')
 let inputName = popup.querySelector('.popup__text_type_name');
 let inputAbout = popup.querySelector('.popup__text_type_about');
 let savePopupButton = popup.querySelector('.popup__btn');
 
 
-function togglePopap () {
+function togglePopap (evt) {
+    console.log('ok')
+    evt.preventDefault();
     popup.classList.toggle('popup_opened')
     inputName.value = heading.textContent;
     inputAbout.value = headingDescription.textContent;
 }
+
+
 
 function closePopup (evt) {
     if(evt.target === evt.currentTarget) {
@@ -20,15 +26,15 @@ function closePopup (evt) {
     }
 }
 
-function savePopup(evt) {
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     heading.textContent = inputName.value;
     headingDescription.textContent = inputAbout.value;
     closePopup(evt);
 }
 
 
-
-savePopupButton.addEventListener('click', savePopup);
+formElement.addEventListener('submit', formSubmitHandler);
 popupOpenButton.addEventListener('click', togglePopap)
 popupCloseButton.addEventListener('click', togglePopap)
 popup.addEventListener('click', closePopup)
